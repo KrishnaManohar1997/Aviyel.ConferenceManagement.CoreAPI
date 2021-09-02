03c3ff75d7bf922f93e692f33b29d759cbc1c4d0
Krishna Manohar <krishnamanohar1997@gmail.com>
2021-09-02 23:35:24 +0530

    Add Talk App

---
+++ talk/models/talk_model.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from common.models import BaseModel
+from conference.models import Conference
+from user.models import User
+
+
+class Talk(BaseModel):
+    title = models.CharField(max_length=32, blank=True, null=True, db_index=True)
+    description = models.CharField(max_length=512, blank=True, null=True)
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(blank=False, null=False)
+    duration = models.DurationField(blank=False, null=False)
+    speakers = models.ManyToManyField(User, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Talk"
+        managed = True
+        verbose_name_plural = "Talks"
+        db_table = "Talk"
