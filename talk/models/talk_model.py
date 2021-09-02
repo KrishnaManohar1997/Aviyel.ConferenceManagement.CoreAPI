5410ffe0e935a533418af9da1f2b62f606e5b52a
Krishna Manohar <krishnamanohar1997@gmail.com>
2021-09-02 23:47:27 +0530

    Added Related Names for better understanding

--- talk/models/talk_model.py
+++ talk/models/talk_model.py
@@ -8,11 +8,13 @@ from user.models import User
 class Talk(BaseModel):
     title = models.CharField(max_length=32, blank=True, null=True, db_index=True)
     description = models.CharField(max_length=512, blank=True, null=True)
-    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+    conference = models.ForeignKey(
+        Conference, on_delete=models.CASCADE, related_name="talks"
+    )
     start_date = models.DateTimeField(blank=False, null=False)
     duration = models.DurationField(blank=False, null=False)
-    speakers = models.ManyToManyField(User, on_delete=models.CASCADE)
-    participants = models.ManyToManyField(User, on_delete=models.CASCADE)
+    speakers = models.ManyToManyField(User, related_name="speakers")
+    participants = models.ManyToManyField(User, related_name="participants")
 
     def __str__(self):
         return self.title
